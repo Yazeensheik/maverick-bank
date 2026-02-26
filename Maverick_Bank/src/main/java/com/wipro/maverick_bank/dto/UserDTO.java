242ae3d6924b65a9628f4ec09fe4fd28439c6cc3
Yazeensheik <yazeenkhansheik@gmail.com>
2026-02-27 00:03:50 +0530

    Added application properties

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/dto/UserDTO.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/dto/UserDTO.java
@@ -2,50 +2,50 @@ package com.wipro.maverick_bank.dto;
 
 public class UserDTO {
 
-    private Long id;
-    private String username;
-    private String role;
-    private boolean active;
+	private Long id;
+	private String username;
+	private String role;
+	private boolean active;
 
-    public UserDTO() {
-    }
+	public UserDTO() {
+	}
 
-    public UserDTO(Long id, String username, String role, boolean active) {
-        this.id = id;
-        this.username = username;
-        this.role = role;
-        this.active = active;
-    }
+	public UserDTO(Long id, String username, String role, boolean active) {
+		this.id = id;
+		this.username = username;
+		this.role = role;
+		this.active = active;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getRole() {
-        return role;
-    }
+	public String getRole() {
+		return role;
+	}
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+	public void setRole(String role) {
+		this.role = role;
+	}
 
-    public boolean isActive() {
-        return active;
-    }
+	public boolean isActive() {
+		return active;
+	}
 
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 }
