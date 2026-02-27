107b936d0d1a21cdb4fef3e9cd2447510d786ef1
Yazeen Khan <95301732+Yazeensheik@users.noreply.github.com>
2026-02-27 23:40:44 +0530

    Add repository annotation and findByUserId method
    Added @Repository annotation and a method to fetch customer profile by user ID.

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
@@ -3,10 +3,13 @@ package com.wipro.maverick_bank.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wipro.maverick_bank.entity.CustomerProfile;
 
+@Repository
 public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
 
+    // Fetch customer profile using user id
     Optional<CustomerProfile> findByUserId(Long userId);
 }
