74a9da90d82ac9566742ae13f724c644773d6e35
Yazeen Khan <95301732+Yazeensheik@users.noreply.github.com>
2026-02-28 00:41:36 +0530

    Update UserRepository.java

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/UserRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.wipro.maverick_bank.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wipro.maverick_bank.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // Used during login (AuthService)
     Optional<User> findByUsername(String username);
 }
