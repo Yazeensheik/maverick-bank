3cdff58be6e6834b40d5a8f60c5624b7159ffd8e
Yazeen Khan <95301732+Yazeensheik@users.noreply.github.com>
2026-02-27 23:36:35 +0530

    Refactor EmployeeProfileRepository to extend JpaRepository

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/EmployeeProfileRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/EmployeeProfileRepository.java
@@ -1,5 +1,12 @@
 package com.wipro.maverick_bank.repository;
 
-public class EmployeeProfileRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wipro.maverick_bank.entity.EmployeeProfile;
+
+public interface EmployeeProfileRepository extends JpaRepository<EmployeeProfile, Long> {
+
+    Optional<EmployeeProfile> findByUserId(Long userId);
 }
