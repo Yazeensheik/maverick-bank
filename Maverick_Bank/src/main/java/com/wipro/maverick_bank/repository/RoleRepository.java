302f44b9915743a7d144e6829195469a29024c8b
Yazeen Khan <95301732+Yazeensheik@users.noreply.github.com>
2026-02-28 00:40:48 +0530

    Update RoleRepository.java

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/RoleRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/RoleRepository.java
@@ -3,10 +3,13 @@ package com.wipro.maverick_bank.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.wipro.maverick_bank.entity.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    // Used to fetch CUSTOMER / EMPLOYEE / ADMIN roles
     Optional<Role> findByName(String name);
 }
