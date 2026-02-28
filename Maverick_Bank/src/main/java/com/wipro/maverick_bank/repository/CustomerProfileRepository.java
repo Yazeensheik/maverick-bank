5d04c6ca61a45539cf4d53a4fa169e113d1a0086
Yazeensheik <yazeenkhansheik@gmail.com>
2026-02-28 20:10:36 +0530

    JWT auth module and ignore Eclipse IDE files

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
@@ -1,15 +1,5 @@
 package com.wipro.maverick_bank.repository;
 
-import java.util.Optional;
+public class CustomerProfileRepository {
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.wipro.maverick_bank.entity.CustomerProfile;
-
-@Repository
-public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
-
-    // Fetch customer profile using user id
-    Optional<CustomerProfile> findByUserId(Long userId);
 }
--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/EmployeeProfileRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/EmployeeProfileRepository.java
@@ -1,15 +1,5 @@
 package com.wipro.maverick_bank.repository;
 
-import java.util.Optional;
+public class EmployeeProfileRepository {
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.wipro.maverick_bank.entity.EmployeeProfile;
-
-@Repository
-public interface EmployeeProfileRepository extends JpaRepository<EmployeeProfile, Long> {
-
-    // Fetch employee profile using user id
-    Optional<EmployeeProfile> findByUserId(Long userId);
 }
