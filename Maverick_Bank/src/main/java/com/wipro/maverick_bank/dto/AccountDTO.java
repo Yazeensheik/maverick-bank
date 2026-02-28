9bacc80ab724177429c4c9f7352e2a8326ccc426
supriyapanda214 <jinni020104@gmail.com>
2026-03-01 00:21:20 +0530

    Updated Account and CustomerProfile mapping and added CustomerProfileRepository

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/dto/AccountDTO.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/dto/AccountDTO.java
@@ -1,20 +1,82 @@
 package com.wipro.maverick_bank.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.wipro.maverick_bank.entity.CustomerProfile;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class AccountDTO {
 
 	private String accountNumber;
 	private String accountType;
 	private Double balance;
 	private String status;
-
 	private Long accountId;
+	private Long customerProfileId;
+
+	public AccountDTO() {
+	}
+
+	public AccountDTO(String accountNumber, String accountType, Double balance, String status, Long accountId,
+			Long customerProfileId) {
+
+		this.accountNumber = accountNumber;
+		this.accountType = accountType;
+		this.balance = balance;
+		this.status = status;
+		this.accountId = accountId;
+		this.customerProfileId = customerProfileId;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public Long getCustomerProfileId() {
+		return customerProfileId;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public void setCustomerProfileId(Long customerProfileId) {
+		this.customerProfileId = customerProfileId;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "customer_profile_id")
+	private CustomerProfile customerProfile;
 }
--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/repository/CustomerProfileRepository.java
@@ -1,5 +1,16 @@
+
 package com.wipro.maverick_bank.repository;
 
-public class CustomerProfileRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wipro.maverick_bank.entity.CustomerProfile;
+
+@Repository
+public interface CustomerProfileRepository extends JpaRepository<CustomerProfile, Long> {
+
+	//Optional<CustomerProfile> findByEmail(String email);
 
 }
