c780bdd34d05e10525fda31020a101bf726bbd54
supriyapanda214 <jinni020104@gmail.com>
2026-03-01 16:47:14 +0530

    Local changes

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/entity/Role.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/entity/Role.java
@@ -3,7 +3,7 @@ package com.wipro.maverick_bank.entity;
 import jakarta.persistence.*;
 
 @Entity
-<<<<<<< Updated upstream
+
 @Table(name = "roles")
 public class Role {
 
@@ -32,39 +32,5 @@ public class Role {
     public void setName(String name) {
         this.name = name;
     }
-=======
-@Table(name="roles")
-public class Role{
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long roleId;
-	
-	@Column(unique=true, nullable=false)
-	private String roleName; //CUSTOMER, EMPLOYEE, ADMIN
-	
-	@OneToMany(mappedBy="role")
-	private List<User> users;
 
-	/*
-	 * @Table(name = "roles") public class Role {
-	 * 
-	 * @Id
-	 * 
-	 * @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
-	 * 
-	 * @Column(unique = true, nullable = false) private String name; // CUSTOMER,
-	 * EMPLOYEE, ADMIN
-	 * 
-	 * public Role() {}
-	 * 
-	 * public Role(String name) { this.name = name; }
-	 * 
-	 * public Long getId() { return id; }
-	 * 
-	 * public String getName() { return name; }
-	 * 
-	 * public void setName(String name) { this.name = name; }
-	 */
->>>>>>> Stashed changes
 }
