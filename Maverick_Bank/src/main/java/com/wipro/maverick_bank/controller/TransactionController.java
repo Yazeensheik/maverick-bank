a7213270eb36c76bb3417012be2c27dc74070815
geethanjali <geethu.panchadi@gmail.com>
2026-03-02 11:29:25 +0530

    adding the controller files

--- Maverick_Bank/src/main/java/com/wipro/maverick_bank/controller/TransactionController.java
+++ Maverick_Bank/src/main/java/com/wipro/maverick_bank/controller/TransactionController.java
@@ -1,10 +1,19 @@
 package com.wipro.maverick_bank.controller;
 
-import com.wipro.maverick_bank.dto.*;
-import com.wipro.maverick_bank.service.TransactionService;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wipro.maverick_bank.dto.DepositRequestDTO;
+import com.wipro.maverick_bank.dto.TransactionResponseDTO;
+import com.wipro.maverick_bank.dto.TransferRequestDTO;
+import com.wipro.maverick_bank.dto.WithdrawRequestDTO;
+import com.wipro.maverick_bank.service.TransactionService;
 
 @RestController
 @RequestMapping("/api/transactions")
