248597e9b4803c84dab8d90d287053b46df71466
supriyapanda214 <jinni020104@gmail.com>
2026-03-05 12:47:47 +0530

    Frontend: account-list page with AJAX service

--- Maverick_Bank/src/main/resources/static/js/services/accountService.js
+++ Maverick_Bank/src/main/resources/static/js/services/accountService.js
@@ -24,3 +24,28 @@ function createAccount(accountData, successCallback, errorCallback) {
 
     xhr.send(JSON.stringify(accountData));
 }
+
+function getAccountsByCustomer(customerId, successCallback, errorCallback) {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "/api/v1/accounts/customer/" + customerId, true);
+
+    xhr.onreadystatechange = function() {
+
+        if (xhr.readyState === 4) {
+
+            if (xhr.status === 200) {
+
+                var response = JSON.parse(xhr.responseText);
+                successCallback(response);
+
+            } else {
+
+                errorCallback(xhr.responseText);
+            }
+        }
+    };
+
+    xhr.send();
+}
